3e93f55441ccbc73f80e9e9e3a552e070a031343
Lonquid <Lonquid8@gmail.com>
2025-04-22 21:22:16 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ app.get('/ping', (req, res) => {
 });
 
 // Start the Express server on port 3000 (or any other port)
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 client.once('ready', () => {
